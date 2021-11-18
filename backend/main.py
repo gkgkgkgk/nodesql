df043dfdc04a6514d69e8c00b5170fa7e30ccee6
gkgkgkgk <gkepets@gmail.com>
2021-11-17 23:33:58 -0500

    backend work

--- backend/main.py
+++ backend/main.py
@@ -5,28 +5,42 @@ from sqlalchemy.sql.functions import rank
 
 #import flask
 from flask import Flask
-from flask_restful import Api, Resource
+from flask import request
+from flask import jsonify
 
 # TODO:
 # - parse the node links
 # - generate sql query
 # - deynamically create classes based on the table names
 
+
 base = declarative_base()
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine("mysql+pymysql://root:gkgkgkgk@localhost/class")
 db = Session(engine)
 base.metadata.create_all(engine)
 conn = engine.connect()
 
+class sailors(base):
+    __tablename__ = "sailors"
+    sid = Column(Integer, primary_key=True)
+    sname = Column(String)
+    rating = Column(Integer)
+    age = Column(Integer)
+
+
 app = Flask(__name__)
-api = Api(app)
-
-class Main(Resource):
-    def post(self):
-        print('post')
-        return "hi"
 
 
-api.add_resource(Main, '/')
+def generateQuery(nodes, links):
+    result = db.query(func.avg(sailors.age)).filter(sailors.rating == 10).scalar()
+    query = str(db.query(func.avg(sailors.age)).filter(sailors.rating == 10))
+    return result, query
+
+
+@app.route('/', methods=['POST'])
+def main():
+    result, query = generateQuery(request.json['nodes'], request.json['links'])
+    return jsonify({"result": result, "query": query})
+
 
 app.run(debug=True)
